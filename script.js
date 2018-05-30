a0b7836679dae9f99ae85050f0efb1711c04b44e
Brett Markowitz <batting1000@comcast.net>
2018-05-30 12:12:40 -0400

    Fixed issue with timer speeding up when clicking start multiple times

--- script.js
+++ script.js
@@ -5,22 +5,35 @@ const start = document.querySelector('.start');
 const pause = document.querySelector('.pause');
 const restart = document.querySelector('.restart');
 
+const model = {
+    running: false,
+    interval: undefined,
+    countdown: function() {
+
+        if(view.currentSeconds === 0) {
+            view.currentSeconds = 59;
+            view.currentMinutes -= 1;
+            view.updateTimerDisplay();
+
+        }
+        else if(view.currentSeconds < 10) {
+            view.currentSeconds -= 1;
+            view.currentSeconds = "0" + view.currentSeconds;
+            view.updateTimerDisplay();
+        }
+        else {
+            view.currentSeconds -= 1;
+            view.updateTimerDisplay();
+        }
+    },
+}
+
 const controller = {
     startTimer: function() {
-
-        setInterval(function() {
-
-            if(view.currentSeconds === 0) {
-                view.currentSeconds = 59;
-                view.currentMinutes -= 1;
-                view.updateTimerDisplay();
-
-            }
-            else {
-                view.currentSeconds -= 1;
-                view.updateTimerDisplay();
-            }
-        }, 1000);
+        if(model.interval) {
+            clearInterval(model.interval);
+        }
+        model.interval = setInterval(model.countdown, 1000);
     },
 }
 
@@ -31,4 +44,9 @@ const view = {
         minutes.textContent = this.currentMinutes;
         seconds.textContent = this.currentSeconds;
     },
+    setupEventListeners: function() {
+        start.addEventListener('click', controller.startTimer);
+    }
 };
+
+view.setupEventListeners();
